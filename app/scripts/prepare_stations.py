6a1a623800c4b00ef9d67c0171ca17c97ef5a461
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2022-05-02 14:39:03 +0200

    Fix stations download. Simplify workflow. Remove unnecessary options

--- app/scripts/prepare_stations.py
+++ app/scripts/prepare_stations.py
@@ -22,11 +22,12 @@ import json
 import numpy
 import re
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 import time
 from os import environ, path, makedirs, remove, getenv
 from matplotlib import pyplot, dates as mdates
 from datetime import datetime
-from urllib3.exceptions import NewConnectionError
 
 from app import app
 
@@ -35,12 +36,9 @@ from utils import io_utils, parsing
 
 logger = logging.getLogger()
 io_helper = app.io_helper
-FORCE_UPDATE = False
 CLEAN_DEPRECATED_STATIONS = False
 
 REQUESTS_MAX_RETRIES=int(environ.get('REQUESTS_MAX_RETRIES','5'))
-REQUESTS_PAUSE_TIME=int(environ.get('REQUESTS_PAUSE_TIME','5'))
-REQUESTS_INTERVAL=int(environ.get('REQUESTS_INTERVAL','2'))
 
 def main():
     # Input arguments
@@ -52,13 +50,13 @@ def main():
     ''')
     parser.add_argument('-v', '--verbose', help='verbose output (debug loglevel)',
                         action='store_true')
-    parser.add_argument('-f', '--force_update', help='update stations that are already on the disk',
-                        action='store_true')
     parser.add_argument('-c', '--clean_deprecated_stations',
                         help='[http(s) sources only] remove stations that are not available online anymore',
                         action='store_true')
     parser.add_argument('--logfile',
                         help='logfile path. Default: prints logs to the console')
+    parser.add_argument('-f', '--force', help='DEPRECATED',
+                        action='store_true')
     args = parser.parse_args()
 
 
@@ -76,9 +74,6 @@ def main():
         loglevel = logging.DEBUG
     logger.setLevel(loglevel)
 
-    if args.force_update:
-        global FORCE_UPDATE
-        FORCE_UPDATE = True
     if args.clean_deprecated_stations:
         global CLEAN_DEPRECATED_STATIONS
         CLEAN_DEPRECATED_STATIONS = True
@@ -108,18 +103,21 @@ def prepare_stations_for_source(src):
         if k.endswith('.folder'):
             makedirs(v.format(source_id = src['id']), exist_ok=True)
 
-    files = []
+    # remove stations that are on the disk but not any more listed on the http source: simplest way is to remove them
+    # all before downloading them again
+    if CLEAN_DEPRECATED_STATIONS:
+        on_disk = glob.glob(io_helper.paths['stations.data'].format(source_id=src['id'], station_id='*'))
+        for station in on_disk:
+            remove(station) # delete file
+
+    # Retrieve the data.
     if src['list_uri'].startswith("http"):
         r = requests.get(src['list_uri'])
-        unchanged_files_list, new_files_list =_retrieve_stations_data(src, r.json())
-        files = unchanged_files_list + new_files_list
-        if CLEAN_DEPRECATED_STATIONS: # remove stations that are on the disk but not any more listed on the http source
-            on_disk = glob.glob(io_helper.paths['stations.data'].format(source_id=src['id'], station_id='*'))
-            deprecated_stations = list( set(on_disk) - set(files) )
-            for station in deprecated_stations:
-                remove(station) # delete file
-    else:
-        files = glob.glob(io_helper.paths['stations.data'].format(source_id=src['id'], station_id='*'))
+        _retrieve_stations_data(src, r.json())
+
+    # Get the files list
+    # files = []
+    files = glob.glob(io_helper.paths['stations.data'].format(source_id=src['id'], station_id='*'))
 
     # create the stations list in geojson format,
     _generate_stations_list(src, files)
@@ -127,8 +125,6 @@ def prepare_stations_for_source(src):
     # generate graph thumbnails
     _generate_thumbnails(src, files)
 
-    if REQUESTS_INTERVAL > 0:
-        time.sleep(REQUESTS_INTERVAL)
 
 def _retrieve_stations_data(src, stations_list):
     """
@@ -149,40 +145,31 @@ def _retrieve_stations_data(src, stations_list):
         except:
             pass
 
-    unchanged_files_list = []
-    new_files_list = []
     for f in stations_list['features']:
         url = details_uri.format(id=f['properties']['productIdentifier'])
 
         dest_file = io_helper.paths['stations.data'].format(source_id=src['id'],
                                                            station_id = f['properties']['productIdentifier'])
-        if FORCE_UPDATE:
-            logger.debug("Downloading data for station {}".format(f['properties']['productIdentifier']))
-        else:
-            # don't re-download the file if already present on the disk
-            if path.exists(dest_file):
-                unchanged_files_list.append(dest_file)
-                continue
-        myfile = None
-        retries = 0
-        try:
-            while myfile is None and retries < REQUESTS_MAX_RETRIES:
-                try:
-                    myfile = requests.get(url)
-                except (requests.exceptions.ReadTimeout , NewConnectionError) as err:
-                    logger.warning("Error while retrieving {}: {}. Pausing and retrying".format(url, err))
-                    time.sleep(REQUESTS_PAUSE_TIME)
 
-            if myfile.status_code == 200 :
-                open(dest_file, 'wb').write(myfile.content)
-                time.sleep(0.2)
-                new_files_list.append(dest_file)
-            else :
-                raise Exception("Download error: status code is {}".format(myfile.status_code))
-        except Exception as e:
-            logger.warning("Error while retrieving {}: {}".format(url, e))
-            logger.exception(e)
-    return unchanged_files_list, new_files_list
+        # Download files using requests.Retry to handle timeouts and server failures
+        retry_strategy = Retry(
+            total=REQUESTS_MAX_RETRIES,
+            backoff_factor=1,
+            status_forcelist=[429, 500, 502, 503, 504],
+            method_whitelist=["HEAD", "GET", "OPTIONS"]
+        )
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+        http = requests.Session()
+        http.mount("https://", adapter)
+        http.mount("http://", adapter)
+
+        response = http.get(url)
+        response.raise_for_status()
+        if response.status_code == 200:
+            # Write data to file
+            open(dest_file, 'wb').write(response.content)
+        else:
+            logger.warning("Error while retrieving {}".format(url))
 
 
 def _generate_stations_list(src, files_list):
